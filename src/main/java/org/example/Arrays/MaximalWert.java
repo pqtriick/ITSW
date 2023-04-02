f2f672b4899cb59adef246138d72b127f7ca3898
zitronebtw <patickde06@gmail.com>
2023-04-02 21:35:40 +0200

    -

--- src/main/java/org/example/Arrays/MaximalWert.java
+++ src/main/java/org/example/Arrays/MaximalWert.java
@@ -2,9 +2,10 @@ package org.example.Arrays;
 
 public class MaximalWert {
 
-    int wert = 0;
+
 
     public MaximalWert(int[] a) {               //Benötigt einen int Array
+        int wert = a[0];
         for(int i = 0; i < a.length; i++) {     // Prüft die Länge des Arrays und addiert ggf. immer 1 auf i dazu
             if (a[i] > wert) {                  //Schaut ob der Array an der Stelle i größer ist als unser int wert
                 wert = a[i];                    //Wenn Ja, soll er den Wert vom Array übernehmen.
--- src/main/java/org/example/Arrays/MinimalWert.java
+++ src/main/java/org/example/Arrays/MinimalWert.java
@@ -2,9 +2,10 @@ package org.example.Arrays;
 
 public class MinimalWert {
 
-    int num = 100;
 
-    public MinimalWert(int[] a) {                   //Benötigt einen int Array
+
+    public MinimalWert(int[] a) {//Benötigt einen int Array
+        int num = a[0];
         for (int i=0; i < a.length; i++) {          // Prüft die Länge des Arrays und addiert ggf. immer 1 auf i dazu
             if (a[i] < num) {                       //Schaut ob der Array an der Stelle i kleiner ist als int num
                 num = a[i];                         //Wenn Ja, dann soll er num zum kleineren Array setzen
